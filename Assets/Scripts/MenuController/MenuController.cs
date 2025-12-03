edbab7006a18615018626f228363e049ce77b10d
Brandon N. Budhan <h703203058@students.hofstra.univ>
2025-12-03 12:34:16 -0500

    Updated UI elements

--- Assets/Scripts/MenuController/MenuController.cs
+++ Assets/Scripts/MenuController/MenuController.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MenuController : MonoBehaviour
 {
     public GameObject GUI;
     private GameObject titleUI;
+    private GameObject controlsUI;
     private GameObject mainCamera;
     private GameObject titleCamera;
     private GameObject player;
+    private VisualElement root;
 
     void Start()
     {
@@ -16,10 +20,16 @@ public class MenuController : MonoBehaviour
         mainCamera = GameObject.Find("CameraHolder");
         player = GameObject.Find("Player");
         titleUI = GameObject.Find("TitleUI");
+
+        controlsUI = GameObject.Find("ControlsUI");
+        var controlsDoc = controlsUI.GetComponent<UIDocument>();
+        root = controlsDoc.rootVisualElement;
+        
         
 
         GUI.SetActive(false);
         titleUI.SetActive(true);
+        controlsUI.SetActive(true);
         player.SetActive(false);
         mainCamera.SetActive(false);
         titleCamera.SetActive(true);
@@ -32,6 +42,9 @@ public class MenuController : MonoBehaviour
         {
             player.SetActive(true);
             titleUI.SetActive(false);
+            // hide controls container in controls UI
+            var controlsContainer = root.Q<VisualElement>("ControlsContainer");
+            controlsContainer.style.display = DisplayStyle.None;
             GUI.SetActive(true);
             mainCamera.SetActive(true);
             titleCamera.SetActive(false);
--- Assets/Scripts/Player/AddToInventory.cs
+++ Assets/Scripts/Player/AddToInventory.cs
@@ -1,17 +1,25 @@
 using System;
 using UnityEngine;
-using UnityEngine.InputSystem;  // New Input System
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;  // New Input System
 
 public class AddToInventory : MonoBehaviour
 {
     [SerializeField] private Camera cam;  // Assign in Inspector; falls back to Camera.main
+    public UIDocument controlsUI;
     private PlayerInventory playerInventory;
+    private VisualElement interactContainer;
+    
 
     private void Start() {
         playerInventory = GameObject.FindWithTag("Player").GetComponent<PlayerInventory>();
         if (playerInventory == null) {
             Debug.LogError("No inventory found");
         }
+
+        
+        var interactDoc = controlsUI.rootVisualElement;
+        interactContainer = interactDoc.Q<VisualElement>("InteractContainer");
     }
 
     private void Awake()
@@ -24,6 +32,20 @@ public class AddToInventory : MonoBehaviour
         // E key
         if (Input.GetKeyDown(KeyCode.E)) {
             TryDeleteAtScreenPos(Mouse.current.position.ReadValue());
+        }
+
+        Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if ((hit.collider.gameObject.GetComponent<Interactable>() ??
+                hit.collider.gameObject.GetComponentInParent<Interactable>()) 
+                != null)
+            {
+                interactContainer.style.display = DisplayStyle.Flex;
+            } else
+            {
+                interactContainer.style.display = DisplayStyle.None;
+            }
         }
     }
 
@@ -40,13 +62,17 @@ public class AddToInventory : MonoBehaviour
                                         hit.collider.gameObject.GetComponentInParent<Interactable>();
             // Debug.Log("Interactable?: " + interactable);
 
+
+
             if (interactable != null)
             {
                 interactable.Interact();
+                
             }
             else
             {
                 Debug.Log($"Hit {hit.collider.name}, but it's not interactable");
+
             }
         }
     }
